a031e2abc7516ee471ff65c4911339cb6cb958d4
Jeff Liao <jeff123433@hotmail.com>
2021-08-02 09:19:17 +0800

    [學習] 主板頁面使用方式

--- AccountingNote.DBSource/Logger.cs
+++ AccountingNote.DBSource/Logger.cs
@@ -10,6 +10,22 @@ namespace AccountingNote.DBSource
     {
         public static void WriteLog(Exception ex)
         {
+            string msg =
+                    $@" {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}
+                        {ex.ToString()}
+                    ";
+
+            string logPath = "C:\\Logs\\Log.log";
+            string folderPath = System.IO.Path.GetDirectoryName(logPath);
+
+            if (!System.IO.Directory.Exists(folderPath))
+                System.IO.Directory.CreateDirectory(folderPath);
+
+            if (!System.IO.File.Exists(logPath))
+                System.IO.File.Create(logPath);
+
+            System.IO.File.AppendAllText(logPath, msg);
+
             throw ex;
         }
     }
---
+++ AccountingNote/WebForm1.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="AccountingNote.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <p>我很好</p>
+    <p>那你呢?</p>
+</asp:Content>
